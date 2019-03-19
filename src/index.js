2d1e4311bd47af0a18ec690d051f16fe62455635
daddaric <christopher.daddario@espn.com>
2019-03-19 16:00:46 -0400

    conditional display

--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ import React from'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return <div>Latitude:</div>;
+    this.state = { lat: null, errorMessage: null };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //once this callback happens, our state is updated
+        //which will then trigger a render
+        this.setState({ lat: position.coords.latitude })
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>
+    } else {
+      return <div>loading...</div>
+    }
+  }
 }
 
 ReactDOM.render(
